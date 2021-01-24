ca3e7db8503b9bb6c02c45ea0d532c74ff3752b2
flow <flow@enceladus.orbit>
2021-01-24 13:46:46 +0100

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
+import sveltePreprocess from 'svelte-preprocess';
+import { coffeescript, } from 'svelte-preprocess';
+// const mode = process.env.NODE_ENV;
+// const dev = mode === 'development';
 
 const name = pkg.name
 	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
 	.replace(/^\w/, m => m.toUpperCase())
 	.replace(/-\w/g, m => m[1].toUpperCase());
+
+const preprocess = sveltePreprocess({ coffeescript: true, });
 
 export default {
 	input: 'src/index.js',
@@ -14,7 +20,12 @@ export default {
 		{ file: pkg.main, 'format': 'umd', name }
 	],
 	plugins: [
-		svelte(),
+    svelte({
+      preprocess,
+      // dev,
+      // hydratable: true,
+      // emitCss: true
+    }),
 		resolve()
 	]
 };
